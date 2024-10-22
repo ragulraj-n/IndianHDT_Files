c9c5647c3b522f9dfeeeb70b91f834b1072a6899
ragulraj-n <135788153+ragulraj-n@users.noreply.github.com>
2024-10-23 01:44:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,8 +27,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ],[
-            InlineKeyboardButton('✪ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url="https://t.me/Tamil_Movie_Request_7"),
-            InlineKeyboardButton('⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url='https://t.me/+7PKyXYgLC9szZjdl')
+            InlineKeyboardButton('✪ Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url="https://t.me/Movie_Request_Group_24x7"),
+            InlineKeyboardButton('⌬ Mᴏᴠɪᴇ CHANNEL', url=CHNL_LNK)
         ],[
             InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
         ]]
