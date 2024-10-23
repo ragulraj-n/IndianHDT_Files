fcd7949c9522cb95a204bcca925c38664c7f19c6
ragulraj-n <135788153+ragulraj-n@users.noreply.github.com>
2024-10-23 18:46:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+xpdulsbXXmtlNTdl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/indianHDT_movie_group')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/indianHDT')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/gplinks_howtodownload')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'indianHDT') # Support Chat Link Without https:// or @
