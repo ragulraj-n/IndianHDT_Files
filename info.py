c24a42dc4816eacd59f4df7ca7b357d617dc7676
ragulraj-n <135788153+ragulraj-n@users.noreply.github.com>
2024-10-23 18:47:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/indianHDT_movie_group')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/indianHDT')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/gplinks_howtodownload')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'indianHDT') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'indianHDT_movie_group') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
